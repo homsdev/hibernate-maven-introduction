968c52f6196cec4f9f2a8b9e068bb98e969805c9
HomS <obr_dev@protonmail.com>
2021-01-20 13:00:53 -0600

    End of Introduction to Hibernate
    -Reviewed Annotation @Entity, @Embeddable, fetching from DB, and
    inserting data to DB

--- DemoHibernate/src/main/java/com/homsdev/DemoHibernate/Alien.java
+++ DemoHibernate/src/main/java/com/homsdev/DemoHibernate/Alien.java
@@ -1,41 +1,49 @@
 package com.homsdev.DemoHibernate;
 
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+
 
 @Entity
-@Table(name="alien_table")
+@Table(name = "alien_table")
 public class Alien {
-		
+
 	@Id
 	private int aid;
-	@Column(name="alien_name")
-	private String aname;
-	@Transient
+	private AlienName aname;
 	private String acolor;
-	
-	
+
 	public int getAid() {
 		return aid;
 	}
+
 	public void setAid(int aid) {
 		this.aid = aid;
 	}
-	public String getAname() {
+
+	public AlienName getAname() {
 		return aname;
 	}
-	public void setAname(String aname) {
+
+	public void setAname(AlienName aname) {
 		this.aname = aname;
 	}
+
 	public String getAcolor() {
 		return acolor;
 	}
+
 	public void setAcolor(String acolor) {
 		this.acolor = acolor;
 	}
+
+	@Override
+	public String toString() {
+		return "Alien [aid=" + aid + ", aname=" + aname + ", acolor=" + acolor + "]";
+	}
+
 	
-	
+
 }
--- DemoHibernate/src/main/java/com/homsdev/DemoHibernate/App.java
+++ DemoHibernate/src/main/java/com/homsdev/DemoHibernate/App.java
@@ -1,7 +1,5 @@
 package com.homsdev.DemoHibernate;
 
-
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -13,28 +11,33 @@ import org.hibernate.service.ServiceRegistryBuilder;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
-        Alien alien= new Alien();
-        
-        alien.setAid(102);
-        alien.setAname("HomSo");
-        alien.setAcolor("red");
-        
-        Configuration con= new Configuration().configure().addAnnotatedClass(Alien.class);
-        ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry(); 
-        SessionFactory sf= con.buildSessionFactory(reg);
-        Session session= sf.openSession();
-        
-        Transaction tx= session.beginTransaction();
-  
-        session.save(alien);
-  
-        tx.commit();
-        
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		
+
+		Configuration con = new Configuration().configure().addAnnotatedClass(Alien.class);
+		ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+		SessionFactory sf = con.buildSessionFactory(reg);
+		Session session = sf.openSession();
+
+		Transaction tx = session.beginTransaction();
+		
+		AlienName an= new AlienName();
+		an.setfName("HomSo");
+		an.setlName("Razome");
+		an.setmName("Marho");
+		Alien alien = new Alien();
+		alien.setAcolor("green");
+		alien.setAid(101);
+		alien.setAname(an);
+		
+		session.save(alien);
+
+		tx.commit();
+
+		System.out.println(alien.toString());
+
+	}
 }
